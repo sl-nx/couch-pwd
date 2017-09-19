57df815f5e031233ce73fad53a1e5e86b73c4fc1
Bjorn Stromberg <bjorn@sensu.one>
2017-09-19 17:43:56 +0900

    v1.0.0 - 2017-09-19

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,14 @@
+# CHANGELOG
 
+## v1.0.0 - 2017-09-19
+- Updated devDependencies (@bjornstar)
+- Updated tests to work with updated devDependencies (@bjornstar)
+- Update .gitignore to ignore log files (@bjornstar)
+- Update Travis CI config to run node 8 (@bjornstar)
+- Added ESLint (@bjornstar)
+- Updated Travis CI config to run lint before test (@bjornstar)
+- Updated style to match ESLint Standard + semicolons (@bjornstar)
+- Add explicit SHA1 digest in crypto.pbkdf2 calls (@EuAndreh)
 
-##### 0.0.1 - 2014-04-09
-
-- init
+## 0.0.1 - 2014-04-09
+- init (@zemirco)
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var crypto = require('crypto');
 
 var iterations = 10;
@@ -8,57 +7,60 @@ var encoding = 'hex';
 var digest = 'SHA1';
 
 // set iterations to `n`
-exports.iterations = function(n){
-  if (0 === arguments.length) return iterations;
+exports.iterations = function (n) {
+  if (arguments.length === 0) return iterations;
   iterations = n;
 };
 
 // set keylen to `n`
-exports.keylen = function(n){
-  if (0 === arguments.length) return keylen;
+exports.keylen = function (n) {
+  if (arguments.length === 0) return keylen;
   keylen = n;
 };
 
 // set size to `n`
-exports.size = function(n){
-  if (0 === arguments.length) return size;
+exports.size = function (n) {
+  if (arguments.length === 0) return size;
   size = n;
 };
 
 // set encoding to `str`
-exports.encoding = function(str){
-  if (0 === arguments.length) return encoding;
+exports.encoding = function (str) {
+  if (arguments.length === 0) return encoding;
   encoding = str;
 };
 
-exports.hash = function(pwd, salt, cb) {
-
+exports.hash = function (pwd, salt, cb) {
   if (arguments.length === 3) {
-
     // create hash from plain text password and salt
     // hash('secret', 'salt', function(err, hash) {})
+
     if (!pwd) return cb(new Error('password missing'));
     if (!salt) return cb(new Error('salt missing'));
-    crypto.pbkdf2(pwd, salt, iterations, keylen, digest, function(err, hash) {
+
+    crypto.pbkdf2(pwd, salt, iterations, keylen, digest, function (err, hash) {
       if (err) return cb(err);
+
       cb(null, hash.toString(encoding));
     });
-
   } else {
-
     // generate salt and password hash from plain text password
     // hash('secret', function(err, salt, hash) {})
+
     cb = salt;
+
     if (!pwd) return cb(new Error('password missing'));
-    crypto.randomBytes(size, function(err, salt) {
+
+    crypto.randomBytes(size, function (err, salt) {
       if (err) return cb(err);
+
       salt = salt.toString('hex');
-      crypto.pbkdf2(pwd, salt, iterations, keylen, digest, function(err, hash){
+
+      crypto.pbkdf2(pwd, salt, iterations, keylen, digest, function (err, hash) {
         if (err) return cb(err);
+
         cb(null, salt, hash.toString(encoding));
       });
     });
-
   }
-
 };
